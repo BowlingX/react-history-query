a9ef04021da3ccb6d1e0644834a31e224f33732d
David Heidrich <me@bowlingx.com>
2017-07-19 14:36:50 +0200

    fix(tests): fixed initial tests

--- src/lib/components/__tests__/QueryContainerTest.js
+++ src/lib/components/__tests__/QueryContainerTest.js
@@ -6,15 +6,14 @@ import React from 'react';
 import createHistory from 'history/createBrowserHistory';
 import QueryContainer from '../QueryContainer';
 
-export const history = createHistory();
-
+const history = createHistory();
 
 describe('<QueryContainer/>', () => {
   it('should render a connected component', () => {
-
     const app = (
-      <QueryContainer history={history}/>
+      <QueryContainer history={history}>
+        <div>App</div>
+      </QueryContainer>
     );
-
   });
 });
